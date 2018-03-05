731ff58d3a31ecf8e59504d216f76b00e172c27b
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2018-03-05 10:12:57 +0100

    Precompute stops-distances matrix of JourneyPattern

--- src/js/models/journeypattern.js
+++ src/js/models/journeypattern.js
@@ -7,7 +7,7 @@ export default class JourneyPattern {
    * @param  {string} code - Reference code
    * @param  {Array.<Stop>} stops - List of stops
    * @param  {Array.<number>} distances - List of distances of each stop
-   * @param  {?(Line|string)} line - Line which the journey pattern belongs to
+   * @param  {(Line|string)} line - Line which the journey pattern belongs to
    * @param  {number} direction - Direction of the line
    */
   constructor(code, stops, distances, line, direction) {
@@ -16,13 +16,11 @@ export default class JourneyPattern {
     this.distances = distances;
     this.line = line;
     this.direction = direction;
-  }
 
-  /**
-   * Getter for the combined list of stops and distances
-   * @return {Array.<{stop: Stop, distance: number}>} Array of (stop, distance) pairs
-   */
-  get stopsDistances() {
-    return this.stops.map((stop, index) => ({ stop, distance: this.distances[index] }));
+    // Create Array with (stop, distance) pairs
+    this.stopsDistances = this.stops.map((stop, index) => ({
+      stop,
+      distance: this.distances[index],
+    }));
   }
 }
